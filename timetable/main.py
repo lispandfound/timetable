5fd988c52fc2db4e64fd7d86282b0020f568cc4c
Jake <jakefaulkn@gmail.com>
2018-02-10 14:50:57 +1300

    Parse locations into Location classes; interpret those in next subcommand

--- timetable/main.py
+++ timetable/main.py
@@ -1,13 +1,15 @@
-import click
-from timetable import timetable
-import time
-from datetime import datetime, timedelta
 import configparser
 import json
 import os
-from terminaltables import SingleTable
+import time
 from collections import defaultdict
+from datetime import datetime
 from itertools import zip_longest
+
+import click
+from terminaltables import SingleTable
+
+from timetable import timetable
 
 
 def activity_as_str(activity, section, course):
@@ -25,10 +27,13 @@ def render_day(day, activities):
             if da[0].start_time < prev_activity[0].end_time:
                 start = time.strftime('%H:%M', da[0].start_time)
                 if da[2] != prev_activity[2]:
-                    rendered_activities[-1] = f'{activity_as_str(*prev_activity)}\n{activity_as_str(*da)}'
+                    rendered_activities[
+                        -1] = f'{activity_as_str(*prev_activity)}\n{activity_as_str(*da)}'
                 i += 1
                 continue
-            time_delta = int(time.mktime(da[0].start_time) - time.mktime(prev_activity[0].end_time))
+            time_delta = int(
+                time.mktime(da[0].start_time) -
+                time.mktime(prev_activity[0].end_time))
             rendered_activities.extend([' '] * (time_delta // 3600))
         else:
             start = time.mktime(time.strptime('08:00', '%H:%M'))
@@ -46,9 +51,7 @@ def parse_iso_date(datestring):
 
 
 def parse_config(filename):
-    converters = {
-        'date': parse_iso_date
-    }
+    converters = {'date': parse_iso_date}
     parser = configparser.ConfigParser(converters=converters)
     with open(filename, 'r') as infile:
         parser.read_file(infile)
@@ -62,7 +65,8 @@ def get_courses(config):
             course_name = section[7:]
             course_year = config[section].getint('year')
             course_semester = config[section].getint('semester')
-            course = timetable.Course(course_name, course_year, course_semester)
+            course = timetable.Course(course_name, course_year,
+                                      course_semester)
             courses.append(course)
     return courses
 
@@ -71,12 +75,18 @@ def get_allocated_activity(config, course, section_name, default=1):
     section_config_name = f'{course.title}/{section_name}'
     act_id = default
     if config.has_section(section_config_name):
-        act_id = config[section_config_name].getint('allocated_activity', default)
+        act_id = config[section_config_name].getint('allocated_activity',
+                                                    default)
     return max(act_id - 1, 0)
 
 
 @click.group()
-@click.option('--config-path', envvar='TIMETABLE_CONFIG_PATH', type=click.Path(), help='Path to configuration (config, data.json).', required=True)
+@click.option(
+    '--config-path',
+    envvar='TIMETABLE_CONFIG_PATH',
+    type=click.Path(),
+    help='Path to configuration (config, data.json).',
+    required=True)
 @click.pass_context
 def cli(ctx, config_path):
     ''' View and manage your UC timetable. '''
@@ -84,13 +94,14 @@ def cli(ctx, config_path):
         os.makedirs(config_path)
 
     config = parse_config(os.path.join(config_path, 'config'))
-    courses = {(course.title, course.year, course.semester): course for course in get_courses(config)}
+    courses = {(course.title, course.year, course.semester): course
+               for course in get_courses(config)}
     activities = defaultdict(list)
     json_path = os.path.join(config_path, 'data.json')
     if os.path.exists(json_path):
         with open(json_path, 'r') as infile:
-            json_config = json.load(infile,
-                                    object_hook=timetable.timetable_load_hook)
+            json_config = json.load(
+                infile, object_hook=timetable.timetable_load_hook)
         for course in json_config:
             if not isinstance(course, timetable.Course):
                 continue
@@ -102,8 +113,10 @@ def cli(ctx, config_path):
         if course.activities == {}:
             course.fetch_details()
         for section_name, section_activities in course.activities.items():
-            allocated_activity = get_allocated_activity(config, course, section_name)
-            if len(section_activities) > 0 and allocated_activity < len(section_activities):
+            allocated_activity = get_allocated_activity(
+                config, course, section_name)
+            if len(section_activities) > 0 and allocated_activity < len(
+                    section_activities):
                 a = section_activities[allocated_activity]
                 activities[a.day].append((a, section_name, course))
 
@@ -114,44 +127,66 @@ def cli(ctx, config_path):
         json.dump(list(courses.values()), out, cls=timetable.TimetableEncoder)
 
 
-@cli.command('timetable')
+@cli.command('show')
 @click.pass_context
 def show_timetable(ctx):
     ''' Show your weekly timetable. '''
-    sorted_days = sorted(ctx.obj['activities'].items(), key=lambda kv: timetable.Activity.days[kv[0]])
-    rendered_days = [['Times', '08:00 - 09:00', '09:00 - 10:00', '10:00 - 11:00', '11:00 - 12:00', '12:00 - 13:00', '13:00 - 14:00', '14:00 - 15:00', '16:00 - 17:00', '17:00 - 18:00']]
+    sorted_days = sorted(
+        ctx.obj['activities'].items(),
+        key=lambda kv: timetable.Activity.days[kv[0]])
+    rendered_days = [[
+        'Times', '08:00 - 09:00', '09:00 - 10:00', '10:00 - 11:00',
+        '11:00 - 12:00', '12:00 - 13:00', '13:00 - 14:00', '14:00 - 15:00',
+        '16:00 - 17:00', '17:00 - 18:00'
+    ]]
     for day, day_activities in sorted_days:
         rendered_activities = render_day(day, day_activities)
         rendered_days.append(rendered_activities)
 
-    tabled_activities = zip_longest(*[list(day) for day in rendered_days], fillvalue='')
+    tabled_activities = zip_longest(
+        *[list(day) for day in rendered_days], fillvalue='')
     print(SingleTable(list(tabled_activities)).table)
 
 
 @cli.command('next')
-@click.option('--show-time', is_flag=True, default=False, help='Show the time to your next class.')
+@click.option(
+    '--show-time',
+    is_flag=True,
+    default=False,
+    help='Show the time to your next class.')
+@click.option(
+    '--at',
+    help=
+    'Set the date and time (in YYYY-MM-DD HH:MM format) to find next class for.',
+    default=None)
 @click.pass_context
-def show_next(ctx, show_time):
+def show_next(ctx, show_time, at):
     ''' Show your next class. '''
     days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-    today = datetime.now()
+    if at is None:
+        today = datetime.now()
+    else:
+        today = datetime.strptime(at, '%Y-%m-%d %H:%M')
+
+    if today.weekday() > 4:
+        return
+
     week_day = days[today.weekday()]
 
     next_classes = []
-    if today.weekday() < 4:
-        days_activities = sorted(ctx.obj['activities'][week_day])
-        i = 0
-        while i < len(days_activities):
-            next_activity = days_activities[i]
-            start = next_activity[0].start_time
-            start_datetime = today.replace(hour=start.tm_hour, minute=start.tm_min)
-            if len(next_classes) > 0 and start == next_classes[-1][0].start_time:
-                next_classes.append(next_activity)
-            elif len(next_classes) > 0 and start != next_classes[-1][0].start_time:
-                break
-            elif today < start_datetime:
-                next_classes = [next_activity]
-            i += 1
+    days_activities = sorted(ctx.obj['activities'][week_day])
+    i = 0
+    while i < len(days_activities):
+        next_activity = days_activities[i]
+        start = next_activity[0].start_time
+        start_datetime = today.replace(hour=start.tm_hour, minute=start.tm_min)
+        if len(next_classes) > 0 and start == next_classes[-1][0].start_time:
+            next_classes.append(next_activity)
+        elif len(next_classes) > 0 and start != next_classes[-1][0].start_time:
+            break
+        elif today < start_datetime:
+            next_classes = [next_activity]
+        i += 1
 
     if len(next_classes) == 0:
         return
@@ -161,8 +196,13 @@ def show_next(ctx, show_time):
         print(start_datetime - today)
     else:
         for activity, title, course in next_classes:
-            print(f'{course.title} - {title} @ {activity.start}: {activity.location}')
-
+            valid_location = next((loc for loc in activity.location
+                                   if loc.valid_for(today)), None)
+            if valid_location is None:
+                continue
+            print(
+                f'{course.title} - {title} @ {activity.start}: {valid_location.name}'
+            )
 
 
 def main():
--- timetable/timetable.py
+++ timetable/timetable.py
@@ -1,19 +1,107 @@
 import functools
-import requests
-import time
 import json
-from bs4 import BeautifulSoup
-from bs4 import NavigableString
+import re
+import time
+from datetime import datetime
 
+import requests
+from bs4 import BeautifulSoup, NavigableString
 
 UC_URL = 'http://www.canterbury.ac.nz/courseinfo/GetCourseDetails.aspx'
+DATE_RE = r"(\d{1,2}\/\d{1,2})(-(\d{1,2}\/\d{1,2}))?"
+LOCATION_RE = r"^(.+)?\((.+)?\)?$"
+
+
+def parse_short_date(datestring, year):
+    tokens = datestring.split('/')
+    day = int(tokens[0])
+    month = int(tokens[1])
+    return datetime(year, month, day)
+
+
+class Location:
+    def __init__(self, location_string, year):
+        self.name = None
+        self.valid_dates = []
+        self.year = year
+        if location_string is not None:
+            self._from_string(location_string)
+
+    def _from_string(self, location_string):
+        location_tokenized = re.match(LOCATION_RE, location_string)
+        if not location_tokenized:
+            self.name = location_string
+            return
+        location_string = location_tokenized.group(1)
+        date_string = location_tokenized.group(2)
+
+        self.name = location_string.strip()
+
+        dates = date_string.split()
+        for date in dates:
+            date_match = re.search(DATE_RE, date)
+            if date_match is None:
+                raise ValueError(
+                    f'repr(location_string) is not a valid location.')
+            elif date_match.group(2) is None or date_match.group(3) is None:
+                self.valid_dates.append(
+                    parse_short_date(date_match.group(1), self.year))
+            else:
+                self.valid_dates.append(
+                    (parse_short_date(date_match.group(1), self.year),
+                     parse_short_date(date_match.group(3), self.year)))
+
+    def valid_for(self, date):
+        if self.valid_dates == []:
+            return True
+
+        for valid_date in self.valid_dates:
+            start_date = None
+            end_date = None
+            if type(valid_date) == datetime:
+                start_date = valid_date
+                end_date = valid_date
+            else:
+                start_date, end_date = valid_date
+            adjusted_end_date = end_date.replace(hour=23, minute=59, second=59)
+            if start_date <= date <= adjusted_end_date:
+                return True
+
+        return False
+
+    def __repr__(self):
+        class_name = self.__class__.__name__
+        return f'{class_name}({self.name}, {self.valid_dates})'
+
+    def as_json(self):
+        json_dates = []
+        for valid_date in self.valid_dates:
+            if type(valid_date) == tuple:
+                start, end = valid_date
+                json_dates.append((start.strftime('%Y-%m-%d'),
+                                   end.strftime('%Y-%m-%d')))
+            else:
+                json_dates.append(valid_date.strftime('%Y-%m-%d'))
+
+        return {
+            '__location__': True,
+            'name': self.name,
+            'year': self.year,
+            'valid_dates': json_dates
+        }
 
 
 @functools.total_ordering
 class Activity:
     '''An activity represents a single activity on the timetable. This could
     be a lecture or a tutorial, as an example.'''
-    days = {'Monday': 0, 'Tuesday': 1, 'Wednesday': 3, 'Thursday': 4, 'Friday': 5}
+    days = {
+        'Monday': 0,
+        'Tuesday': 1,
+        'Wednesday': 3,
+        'Thursday': 4,
+        'Friday': 5
+    }
 
     def __init__(self, activity_id, day, start_time, end_time, location):
         '''Initialize activity class.'''
@@ -22,7 +110,6 @@ class Activity:
         self.start_time = start_time
         self.end_time = end_time
         self.location = location
-
 
     @property
     def start(self):
@@ -34,8 +121,10 @@ class Activity:
 
     def __eq__(self, other):
         '''Returns True if two Activities are the same.'''
-        return ((self.activity_id, self.day, self.start_time, self.end_time, self.location)
-                == (other.activity_id, other.day, other.start_time, other.end_time, other.location))
+        return ((self.activity_id, self.day, self.start_time, self.end_time,
+                 self.location) == (other.activity_id, other.day,
+                                    other.start_time, other.end_time,
+                                    other.location))
 
     def __lt__(self, other):
         '''Returns True if one Activity occurs before another in the timetable.'''
@@ -59,18 +148,46 @@ class Activity:
         }
 
 
-def parse_activity(activity_element):
-    activity_id = activity_element.find_next('td', attrs={'data-title': 'Activity'})
-    activity_day = activity_element.find_next('td', attrs={'data-title': 'Day'})
-    activity_time = activity_element.find_next('td', attrs={'data-title': 'Time'})
-    activity_location = activity_element.find_next('td', attrs={'data-title': 'Location'})
-    [start_time, end_time] = [time.strptime(t.strip(), '%H:%M') for t in activity_time.text.strip().split('-')]
+def parse_activity(activity_element, year):
+    activity_id = activity_element.find_next(
+        'td', attrs={
+            'data-title': 'Activity'
+        })
+    activity_day = activity_element.find_next(
+        'td', attrs={
+            'data-title': 'Day'
+        })
+    activity_time = activity_element.find_next(
+        'td', attrs={
+            'data-title': 'Time'
+        })
+    activity_location = activity_element.find_next(
+        'td', attrs={
+            'data-title': 'Location'
+        })
+
+    for br in activity_location.find_all("br"):
+        br.replace_with("\n")
+
+    locations = []
+
+    for location in activity_location.text.split('\n'):
+        location = location.strip()
+        if location != '':
+            locations.append(Location(location, year))
+
+    [start_time, end_time] = [
+        time.strptime(t.strip(), '%H:%M')
+        for t in activity_time.text.strip().split('-')
+    ]
     activity_id = int(activity_id.text.strip())
-    return Activity(activity_id, activity_day.text.strip(), start_time, end_time, activity_location.text.strip())
+    return Activity(activity_id, activity_day.text.strip(), start_time,
+                    end_time, locations)
 
 
 class Course:
     ''' A course represents a single course'''
+
     def __init__(self, title, year, semester, activities=None):
         '''Initialize course class.'''
         self.activities = activities or {}
@@ -91,8 +208,12 @@ class Course:
         '''Fetch course details and update activities.'''
         with requests.get(self.to_url()) as r:
             soup = BeautifulSoup(r.content, 'html.parser')
-            course_table = soup.select_one('table.table.table-hover.table-bordered.table-condensed.cf')
-            section_headers = [e.find_next_sibling('tbody') for e in course_table.find_all('thead', class_='cf')]
+            course_table = soup.select_one(
+                'table.table.table-hover.table-bordered.table-condensed.cf')
+            section_headers = [
+                e.find_next_sibling('tbody')
+                for e in course_table.find_all('thead', class_='cf')
+            ]
             for section_header in section_headers:
                 section_name = section_header.text.strip()
                 activity_rows = []
@@ -102,7 +223,9 @@ class Course:
                     if row.name and row.name != 'tr':
                         break
                     activity_rows.append(row)
-                activities = [parse_activity(row) for row in activity_rows]
+                activities = [
+                    parse_activity(row, self.year) for row in activity_rows
+                ]
                 self.activities[section_name] = activities
 
     def as_json(self):
@@ -117,18 +240,38 @@ class Course:
 
 def timetable_load_hook(dct):
     if '__course__' in dct:
-        return Course(dct['title'], dct['year'], dct['semester'], activities=dct['activities'])
+        return Course(
+            dct['title'],
+            dct['year'],
+            dct['semester'],
+            activities=dct['activities'])
     elif '__activity__' in dct:
         start = time.strptime(dct['start'], '%H:%M')
         end = time.strptime(dct['end'], '%H:%M')
         return Activity(dct['id'], dct['day'], start, end, dct['location'])
+    elif '__location__' in dct:
+        dates = dct['valid_dates']
+        loaded_dates = []
+        for date in dates:
+            if type(date) == list:
+                start = datetime.strptime(date[0], '%Y-%m-%d')
+                end = datetime.strptime(date[1], '%Y-%m-%d')
+                loaded_dates.append((start, end))
+            else:
+                loaded_dates.append(datetime.strptime(date, '%Y-%m-%d'))
+
+        loc = Location(None, dct['year'])
+        loc.name = dct['name']
+        loc.valid_dates = loaded_dates
+        return loc
     else:
         return dct
 
 
 class TimetableEncoder(json.JSONEncoder):
     def default(self, obj):
-        if isinstance(obj, Course) or isinstance(obj, Activity):
+        if isinstance(obj, Course) or isinstance(obj, Activity) or isinstance(
+                obj, Location):
             return obj.as_json()
         else:
             return obj
